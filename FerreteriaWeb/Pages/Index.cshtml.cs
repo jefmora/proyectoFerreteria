b24be9a27384ed80e32f3568db656f690117daea
deynon18 <166685032+deynon18@users.noreply.github.com>
2026-06-02 11:29:51 -0600

    Creacion de la App
    se crea la solucion con el proyecto

---
+++ FerreteriaWeb/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FerreteriaWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ FerreteriaWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FerreteriaWeb.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
