3243764f66f2beab620d9dacf4fc0bfb42b38511
jefmora <jefersonmora333@gmail.com>
2026-06-02 21:46:06 -0600

    Pantalla de Inicio
    Se agrega la pantalla de inicio con la plantilla de ferreteria

--- FerreteriaProAvaWeb/FerreteriaWeb/obj/Debug/net10.0/FerreteriaWeb.AssemblyInfo.cs
+++ FerreteriaProAvaWeb/FerreteriaWeb/obj/Debug/net10.0/FerreteriaWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FerreteriaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eae4e74ea4995f2bcdd293bf1528c7ce2f5dd2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb7a57cecc4beb43ca2a5d28d11c5bcba3ac4cb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("FerreteriaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FerreteriaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
