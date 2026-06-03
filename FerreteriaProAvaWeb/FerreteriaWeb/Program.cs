0c105c3612bdffc546f81e6dd35b40dd9a3cefc9
Jose <jsequeira00582@ufide.ac.cr>
2026-06-03 15:09:10 -0600

    Registrar

--- FerreteriaProAvaWeb/FerreteriaWeb/Program.cs
+++ FerreteriaProAvaWeb/FerreteriaWeb/Program.cs
@@ -3,17 +3,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Registrar IHttpContextAccessor
+builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
+    // The default HSTS value is 30 days.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
@@ -24,6 +29,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Auth}/{action=LoginBasic}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
